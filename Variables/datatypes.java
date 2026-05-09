729e9a4374cfa92e7476cd89dca59bc58849fc4b
Mohit Sharma <ms1361277@gmail.com>
2026-05-09 23:07:19 +0530

    sum data types

---
+++ Variables/datatypes.java
@@ -0,0 +1,31 @@
+public class datatypes {
+    public static void main (String[] args){
+        byte b =8;
+        System.out.println(b);
+
+        char ch ='a';
+        System.out.println(ch);
+
+        short s = 8989;
+        System.out.println(s);
+
+        int i = 89898;
+        System.out.println(i);
+
+        long l = 89898989898L;
+        System.out.println(l);
+
+        float f = 89.89f;
+        System.out.println(f);
+
+        double d = 89.8989;
+        System.out.println(d);
+
+        boolean bool = true;
+        System.out.println(bool);
+
+        String name = "mohit";
+        System.out.println(name);    
+
+    }
+}
