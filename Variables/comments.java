c7aeb980ee86e3ffcd121a84d36a7f94761f3819
Mohit Sharma <ms1361277@gmail.com>
2026-05-09 23:11:13 +0530

    comments

---
+++ Variables/comments.java
@@ -0,0 +1,11 @@
+public class comments {
+    public static void main (String[] args) {
+         //ujjain ke mahakal
+        System.out.println("jai Shree Mahakal")
+
+        /* hello 
+        kya haal 
+        hai 
+        ji */
+    }
+}
